ef8ebc0cea6b3d2d84119d6d55516eaaeb871928
Emeka Okezie <eokezie@nasdng.com>
2023-06-11 17:08:33 +0100

    Update: Created the password indicator component

---
+++ src/components/indicator/Indicator.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { CheckboxI } from '../../utils/data';
+import { useStrength } from '../../hooks/useStrength';
+
+import './Indicator.css';
+
+interface IndicatorProps {
+    options: CheckboxI[]
+};
+
+export const Indicator: React.FC<IndicatorProps> = ({ options }) => {
+    const strength = useStrength({ options });
+
+  return (
+    <div className='indicator'>
+        <span className='indicator__title'>STRENGTH</span>
+        <div className='indicator__data'>
+            <span className='indicator__data-title'>
+                {strength === 'too-weak' ? `${strength}!` : strength}
+            </span>
+            <div className={`indicator__data-grid ${strength}`}>
+                {options.map((option) => (
+                    <div 
+                        key={option.label}
+                        className='indicator__data-grid__item'
+                    />
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Indicator;
