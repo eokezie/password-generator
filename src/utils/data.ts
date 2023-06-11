cdd0120792edba067e52710c5b3fe1c25f07f227
Emeka Okezie <eokezie@nasdng.com>
2023-06-11 16:32:14 +0100

    Created an Utility function for the input fields

---
+++ src/utils/data.ts
@@ -0,0 +1,48 @@
+export interface CheckboxI {
+    id: number;
+    label: string;
+    checked: boolean
+}
+
+export const checkboxInfo: CheckboxI[] = [
+    {id: 1, label: 'Include Uppercase Letters', checked: false},
+    {id: 2, label: 'Include Lowercase Letters', checked: true},
+    {id: 3, label: 'Include Numbers', checked: false},
+    {id: 4, label: 'Include Symbols', checked: false},
+];
+
+export const generatePassword = (checkboxInfo: CheckboxI[], length: number) => {
+    const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()';
+    let passwordOptions = '';
+
+    checkboxInfo.forEach((checkbox) => {
+        if (checkbox.checked) {
+            switch (checkbox.label) {
+                case 'Include Uppercase Letters':
+                    passwordOptions += uppercaseLetters;
+                    break;
+                case 'Include Lowercase Letters':
+                    passwordOptions += lowercaseLetters;
+                    break;
+                case 'Include Numbers':
+                    passwordOptions += numbers;
+                    break;
+                case 'Include Symbols':
+                    passwordOptions += symbols
+                    break;
+            };
+        };
+    });
+
+    let password = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * passwordOptions.length);
+        password += passwordOptions[randomIndex]
+    }
+
+    return password;
+}
